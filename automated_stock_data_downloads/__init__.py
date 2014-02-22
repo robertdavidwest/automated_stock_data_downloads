99460a2918cd7f8c0fce2c4aaf138e429a61a5e3
Robert David West <robertdavidwest@Roberts-MacBook-Air.local>
2014-02-21 21:28:23 -0500

    added setup.py and __init__.py files so that this repository can be installed as a python library by the end user, by running - python setup.py install

---
+++ automated_stock_data_downloads/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+.. module:: __init__.py
+   :synopsis: initialization file for ``automated_stock_data_downloads``
+
+.. moduleauthor:: Robert D. West <robert.david.west@gmail.com>
+
+"""
+import get_stock_data
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='automated_stock_data_downloads',
+      version='0.0.1',
+      description='Download Adjusted Close stock price data and find constituent tickers from Indices',
+      author='Robert D. West',
+      author_email='robert.david.west@gmail.com',
+      url='https://github.com/robertdavidwest/automated_stock_data_downloads',
+      packages=['automated_stock_data_downloads'],
+     )
